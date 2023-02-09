08c61a84b21e315f095189abd1052814ec8174eb
TaeJu <leex9121@umn.edu>
2023-02-08 22:40:25 -0500

    Pushing user-service and ride-service

---
+++ user-service/src/main/java/com/tjcode/userservice/service/RoleService.java
@@ -0,0 +1,17 @@
+package com.tjcode.userservice.service;
+
+import com.tjcode.userservice.entity.Role;
+import com.tjcode.userservice.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleService {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    public Role createNewRole(Role role) {
+        return roleRepository.save(role);
+    }
+}
