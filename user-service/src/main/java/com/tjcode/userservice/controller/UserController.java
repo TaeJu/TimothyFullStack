0ac95637bec67bf866abc43bd2431a81e5425b7b
TaeJu <leex9121@umn.edu>
2023-02-12 22:21:46 -0500

    Committing first User Service and JWT/Spring Security

---
+++ user-service/src/main/java/com/tjcode/userservice/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.tjcode.userservice.controller;
+
+import com.tjcode.userservice.entity.User;
+import com.tjcode.userservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/registerNewUser")
+    public User registerNewUser(@RequestBody User user) {
+        return userService.registerNewUser(user);
+    }
+
+    @GetMapping("/forAdmin")
+    @PreAuthorize("hasRole('Admin')")
+    public String forAdmin() {
+        return "This URL is only accessible to admin";
+    }
+
+    @GetMapping("/forUser")
+    @PreAuthorize("hasAnyRole('Admin','User')")
+    public String forUser() {
+        return "This URL is only accessible to user";
+    }
+}
