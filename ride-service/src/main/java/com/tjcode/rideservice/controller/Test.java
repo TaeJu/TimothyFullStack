e53a42cc3b58b98484e6ba987d0afd9cbf16e285
TaeJu <leex9121@umn.edu>
2023-02-13 21:10:17 -0500

    Pushing Cloud Gateway

--- ride-service/src/main/java/com/tjcode/rideservice/controller/Test.java
+++ ride-service/src/main/java/com/tjcode/rideservice/controller/Test.java
@@ -1,9 +1,11 @@
 package com.tjcode.rideservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/ride-service")
 public class Test {
 
     @GetMapping("/hello")
--- user-service/src/main/java/com/tjcode/userservice/controller/RoleController.java
+++ user-service/src/main/java/com/tjcode/userservice/controller/RoleController.java
@@ -5,9 +5,11 @@ import com.tjcode.userservice.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user-service")
 public class RoleController {
 
     @Autowired
