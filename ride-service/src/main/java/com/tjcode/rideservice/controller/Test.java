45028bc5ed7a087eb86533927d0f905fa70e5320
TaeJu <leex9121@umn.edu>
2023-02-14 20:35:40 -0500

    Creating JWT filter for rider serivce server

--- ride-service/src/main/java/com/tjcode/rideservice/controller/Test.java
+++ ride-service/src/main/java/com/tjcode/rideservice/controller/Test.java
@@ -1,5 +1,6 @@
 package com.tjcode.rideservice.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class Test {
 
     @GetMapping("/hello")
+    @PreAuthorize("hasAnyRole('Admin','User')")
     public String helloWorld() {
         return "Hello World";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "test get call";
     }
 }
