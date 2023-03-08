3a3d97fe4a8c4b612ad32d649d9d9ab10d1fc75d
TaeJu <leex9121@umn.edu>
2023-03-07 21:11:25 -0500

    Adding getAllRider API

--- ride-service/src/main/java/com/tjcode/rideservice/service/RideService.java
+++ ride-service/src/main/java/com/tjcode/rideservice/service/RideService.java
@@ -8,13 +8,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
+@Transactional
 public class RideService {
 
     private static final Logger logger = LoggerFactory.getLogger(RideService.class);
+
+    @PersistenceContext
+    private EntityManager em;
 
     @Autowired
     private RideRequestRepository rideRequestRepository;
@@ -58,5 +67,18 @@ public class RideService {
             rider.setCurrentFillSpot(numAssigned);
             riderRepository.save(rider);
         }
+    }
+
+    public List<Rider> getAllRider() {
+        TypedQuery<Rider> q = em.createQuery("SELECT r FROM Rider r LEFT JOIN FETCH r.requests", Rider.class);
+        List<Rider> riders = q.getResultList();
+
+        em.close();
+
+        for (Rider r : riders) {
+            logger.info(r.getName() + ", " + r.getRequests().stream().map(rs -> rs.getRequestName()).collect(Collectors.joining(", ")));
+        }
+
+        return riders;
     }
 }
