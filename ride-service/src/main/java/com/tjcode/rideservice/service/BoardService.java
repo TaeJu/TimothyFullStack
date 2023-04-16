c102765a5e015fff18d9ab3d3978404cdc4b77e6
TaeJu <leex9121@umn.edu>
2023-04-16 15:43:39 -0400

    Adding Board Feature

--- ride-service/src/main/java/com/tjcode/rideservice/service/BoardService.java
+++ ride-service/src/main/java/com/tjcode/rideservice/service/BoardService.java
@@ -4,7 +4,13 @@ import com.tjcode.rideservice.dto.BoardDTO;
 import com.tjcode.rideservice.entity.BoardEntity;
 import com.tjcode.rideservice.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class BoardService {
@@ -16,5 +22,20 @@ public class BoardService {
         BoardEntity boardEntity = BoardEntity.toSaveEntity(boardDTO);
         boardRepository.save(boardEntity);
         return boardDTO;
+    }
+
+    public List<BoardDTO> findAll(int pageNumber) {
+        Pageable pageable = PageRequest.of(pageNumber,8);
+        Page<BoardEntity> boardEntityList = boardRepository.findAll(pageable);
+        List<BoardDTO> boardDTOList = new ArrayList<>();
+        for (BoardEntity boardEntity : boardEntityList) {
+            boardDTOList.add(BoardDTO.toBoardDTO(boardEntity));
+        }
+
+        return boardDTOList;
+    }
+
+    public BoardDTO findById(Long boardId) {
+        return BoardDTO.toBoardDTO(boardRepository.findById(boardId).get());
     }
 }
